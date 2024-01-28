75c709277203ec563e53841ef9ce44884fbb9291
Julien Kipp <julien@doji-tech.com>
2024-01-28 01:24:58 +0100

    add unet test with larger samples

--- com.doji.diffusers/Tests/Editor/UnetTest.cs
+++ com.doji.diffusers/Tests/Editor/UnetTest.cs
@@ -25,10 +25,24 @@ namespace Doji.AI.Diffusers.Editor.Tests {
 
         private float[] ExpectedOutput {
             get {
-                return TestUtils.LoadFromFile("unet_test_output");
+                return TestUtils.LoadFromFile("unet_test_output_256");
             }
         }
 
+        /// <summary>
+        /// Loads deterministic random samples with shape (1, 4, 64, 64)
+        /// </summary>
+        private float[] SamplesLarge {
+            get {
+                return TestUtils.LoadFromFile("16384_latents");
+            }
+        }
+
+        private float[] ExpectedOutputLarge {
+            get {
+                return TestUtils.LoadFromFile("unet_test_output_16384");
+            }
+        }
         private static Unet LoadUnet() {
             var model = StableDiffusionPipeline.LoadUnet(DiffusionModel.SD_1_5.Name);
             return new Unet(model);
@@ -48,6 +62,22 @@ namespace Doji.AI.Diffusers.Editor.Tests {
             float[] unetOutput = noisePred.ToReadOnlyArray();
 
             CollectionAssert.AreEqual(ExpectedOutput, unetOutput, new FloatArrayComparer(0.00001f));
+        }
+
+        [Test]
+        public void TestUnetLarge() {
+            using Unet unet = LoadUnet();
+
+            int t = 901;
+            using TensorFloat latentInputTensor = new TensorFloat(new TensorShape(1, 4, 64, 64), SamplesLarge);
+            using TensorFloat promptEmbeds = new TensorFloat(new TensorShape(1, 77, 768), PromptEmbeds);
+            using TensorInt timestep = new TensorInt(new TensorShape(1), ArrayUtils.Full(1, t));
+
+            TensorFloat noisePred = unet.ExecuteModel(latentInputTensor, timestep, promptEmbeds);
+            noisePred.MakeReadable();
+            float[] unetOutput = noisePred.ToReadOnlyArray();
+
+            CollectionAssert.AreEqual(ExpectedOutputLarge, unetOutput, new FloatArrayComparer(0.00001f));
         }
     }
 }
