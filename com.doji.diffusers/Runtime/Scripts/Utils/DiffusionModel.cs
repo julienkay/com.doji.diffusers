d50c78937255119cc0c5f65f79c26866be6f5a77
Julien Kipp <julien@doji-tech.com>
2024-02-23 20:39:32 +0100

    add reference to SDXL model hosted on HF

--- com.doji.diffusers/Runtime/Scripts/Utils/DiffusionModel.cs
+++ com.doji.diffusers/Runtime/Scripts/Utils/DiffusionModel.cs
@@ -38,9 +38,18 @@ namespace Doji.AI.Diffusers {
             $"tokenizer/tokenizer_config.json",
             $"tokenizer/vocab.json",
             $"unet/model.onnx",
-            $"unet/model.onnx_data",    // optional
-            $"unet/weights.pb",         // optional
             $"vae_decoder/model.onnx",
+            $"vae_encoder/model.onnx"
+        };
+
+        public static readonly IEnumerable<string> OptionalFiles = new List<string>() {
+            $"text_encoder_2/model.onnx",           // SDXL
+            $"tokenizer_2/merges.txt",              // SDXL
+            $"tokenizer_2/special_tokens_map.json", // SDXL
+            $"tokenizer_2/tokenizer_config.json",   // SDXL
+            $"tokenizer_2/vocab.json",              // SDXL
+            $"unet/model.onnx_data",                // runwayml repo stores it that way
+            $"unet/weights.pb"                      // runwayml repo stores it that way
         };
 
         private const string HF_URL = "https://huggingface.co";
@@ -49,13 +58,14 @@ namespace Doji.AI.Diffusers {
 
         public IEnumerator<(string url, string filePath, bool optional)> GetEnumerator() {
             foreach (string fileName in Files) {
-                bool optional = false;
-                if (fileName == "unet/model.onnx_data" || fileName == "unet/weights.pb") {
-                    optional = true;
-                }
                 string url = $"{HF_URL}/{Name}/resolve/{Revision}/{fileName}";
                 string filePath = Path.Combine("Packages", "com.doji.diffusers", "Runtime", "Resources", Name, $"{fileName}");
-                yield return (url, filePath, optional);
+                yield return (url, filePath, false);
+            }
+            foreach (string fileName in OptionalFiles) {
+                string url = $"{HF_URL}/{Name}/resolve/{Revision}/{fileName}";
+                string filePath = Path.Combine("Packages", "com.doji.diffusers", "Runtime", "Resources", Name, $"{fileName}");
+                yield return (url, filePath, true);
             }
         }
 
@@ -65,15 +75,15 @@ namespace Doji.AI.Diffusers {
 
         public static readonly DiffusionModel SD_1_5 = new DiffusionModel("runwayml/stable-diffusion-v1-5", "onnx");
         public static readonly DiffusionModel SD_2_1 = new DiffusionModel("julienkay/stable-diffusion-2-1");
+        public static readonly DiffusionModel SD_XL_BASE  = new DiffusionModel("stabilityai/stable-diffusion-xl-base-1.0");
         //public static readonly DiffusionModel SD_XL_TURBO = new DiffusionModel("stabilityai/sdxl-turbo");
-        //public static readonly DiffusionModel SD_XL_BASE  = new DiffusionModel("stabilityai/stable-diffusion-xl-base-1.0");
 
         /* all the validated models */
         public static readonly DiffusionModel[] ValidatedModels = new DiffusionModel[] {
                  SD_1_5,
-                 SD_2_1
+                 SD_2_1,
+                 SD_XL_BASE
                  //SD_XL_TURBO,
-                 //SD_XL_BASE,
         };
     }
 }
