abbad1dce063d0da8797fa8c1cb8f5e67b10ebae
Julien Kipp <julien@doji-tech.com>
2024-03-27 14:09:10 +0100

    add Metadata test for PackageVersion

--- com.doji.diffusers/Tests/Editor/MetadataSerializationTest.cs
+++ com.doji.diffusers/Tests/Editor/MetadataSerializationTest.cs
@@ -5,7 +5,7 @@ namespace Doji.AI.Diffusers.Editor.Tests {
     public class MetadataSerializationTest {
 
         [Test]
-        public void Serialize() {
+        public void TestRoundtrip() {
             Metadata metadata = new Metadata() {
                 Model = "TestModel",
                 Pipeline = "TestPipeline",
@@ -24,6 +24,17 @@ namespace Doji.AI.Diffusers.Editor.Tests {
             Assert.That(deserialized, Is.Not.Null);
             Assert.That(deserialized.Comment, Is.Not.Null);
             Assert.AreEqual("TestPrompt", deserialized.Parameters.Prompt.ToString());
+        }
+
+        [Test]
+        public void TestPackageVersion() {
+            string json = "{\"comment\":\"This image was generated using https://github.com/julienkay/com.doji.diffusers\",\"package_version\":\"0.1.0\",\"model\":\"TestModel\",\"pipeline\":\"TestPipeline\",\"sampler\":\"TestSampler\",\"parameters\":{}}";
+            Metadata deserialized = Metadata.Deserialize(json);
+            Assert.That(deserialized, Is.TypeOf(typeof(Metadata)));
+            Assert.That(deserialized, Is.Not.Null);
+            Assert.That(deserialized.PackageVersion, Is.Not.Null);
+            Assert.AreEqual("0.1.0", deserialized.PackageVersion);
+            string packageVersion = System.Diagnostics.FileVersionInfo.GetVersionInfo(typeof(Metadata).Assembly.Location).ProductVersion;
         }
     }
 }
