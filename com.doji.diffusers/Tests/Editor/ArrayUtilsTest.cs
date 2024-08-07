c4490b74f92c5e6717d905778d997d309ef4149c
Julien Kipp <julien@doji-tech.com>
2024-08-07 19:29:19 +0200

    cosmetic changes

--- com.doji.diffusers/Tests/Editor/ArrayUtilsTest.cs
+++ com.doji.diffusers/Tests/Editor/ArrayUtilsTest.cs
@@ -25,7 +25,6 @@ namespace Doji.AI.Diffusers.Editor.Tests {
         [Test]
         public void TestLinspaceFract1() {
             float[] spacedValues = ArrayUtils.Linspace(1, 20, 15, endpoint: false);
-            UnityEngine.Debug.Log(string.Join(", ", spacedValues));
             Assert.That(spacedValues,
                 Is.EqualTo(
                     new float[] { 1.0f, 2.26666667f, 3.53333333f, 4.8f, 6.06666667f, 7.33333333f, 8.6f, 9.86666667f, 11.13333333f, 12.4f, 13.66666667f, 14.93333333f, 16.2f, 17.46666667f, 18.73333333f }
@@ -36,7 +35,6 @@ namespace Doji.AI.Diffusers.Editor.Tests {
         [Test]
         public void TestLinspaceFract2() {
             float[] spacedValues = ArrayUtils.Linspace(1, 20, 15, endpoint: true);
-            UnityEngine.Debug.Log(string.Join(", ", spacedValues));
             Assert.That(spacedValues,
               Is.EqualTo(
                   new float[] { 1.0f, 2.35714286f, 3.71428571f, 5.07142857f, 6.42857143f, 7.78571429f, 9.14285714f, 10.5f, 11.85714286f, 13.21428571f, 14.57142857f, 15.92857143f, 17.28571429f, 18.64285714f, 20.0f }
