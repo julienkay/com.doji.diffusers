175e1fb344230c62321572dffba7cffca33d6d7f
Julien Kipp <julien@doji-tech.com>
2025-04-10 21:59:43 +0200

    bump version

--- com.doji.diffusers/Runtime/AssemblyInfo.cs
+++ com.doji.diffusers/Runtime/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("Run pretrained diffusion models with Unity Sentis")]
 [assembly: AssemblyCompany("Doji Technologies")]
 [assembly: AssemblyProduct("Doji.Diffusers")]
-[assembly: AssemblyCopyright("Copyright © Julien Kipp 2023-2024")]
+[assembly: AssemblyCopyright("Copyright © Julien Kipp 2023-2025")]
 
-[assembly: AssemblyVersion("0.3.4.0")]
-[assembly: AssemblyFileVersion("0.3.4.0")]
-[assembly: AssemblyInformationalVersion("0.3.4")]
+[assembly: AssemblyVersion("0.3.5.0")]
+[assembly: AssemblyFileVersion("0.3.5.0")]
+[assembly: AssemblyInformationalVersion("0.3.5")]
 [assembly: InternalsVisibleTo("Doji.Diffusers.Editor.Tests")]
